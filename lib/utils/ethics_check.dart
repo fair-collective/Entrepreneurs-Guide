dfd1a28d10d89d680a23117d0ab7e3efb3f4965c
Jotto1988 <115623545+Jotto1988@users.noreply.github.com>
2025-11-03 16:51:22 +0200

    Create ethics_check.dart

---
+++ lib/utils/ethics_check.dart
@@ -0,0 +1,7 @@
+class EthicsCheck {
+  static String weekly() => '''
+  ETHICS CHECK: Are you creating real value? 
+  Are you helping, not exploiting? 
+  Is this sustainable for you and your clients?
+  ''';
+}
